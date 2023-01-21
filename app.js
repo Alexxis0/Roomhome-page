6a6f7243e992c6f6eaa15d7771b2b25cbc140829
Alexis Morantin <alexismorantin44390@gmail.com>
2023-01-21 10:43:19 +0100

    carrousel

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-let iconHamburger = document.querySelector('.icon_hamburger');
-let iconHamburgerClose = document.querySelector('.icon_hamburger_close');
-let menuMobile = document.querySelector('.menu')
-console.log(menuMobile);
+const iconHamburger = document.querySelector(".icon_hamburger");
+const iconHamburgerClose = document.querySelector(".icon_hamburger_close");
+const menuMobile = document.querySelector(".menu");
+
+const btnArrowLeft = document.querySelector(".btn_arrow_left");
+const btnArrowRight = document.querySelector(".btn_arrow_right");
+const slideImg = document.querySelectorAll(".slide");
+const slidePara = document.querySelectorAll(".slide_para");
+let count = 0;
+
 iconHamburger.addEventListener("click", menu);
 iconHamburgerClose.addEventListener("click", menu);
 
-function menu () {
+btnArrowLeft.addEventListener("click", slidePrev);
+btnArrowRight.addEventListener("click", slideNext);
+
+function menu() {
     iconHamburgerClose.classList.toggle("icon_hamburger_close_open");
     iconHamburger.classList.toggle("icon_hamburger_delete");
-    menuMobile.classList.toggle('menu_mobile')
+    menuMobile.classList.toggle("menu_mobile");
+}
+
+function slidePrev() {
+    slideImg[count].classList.remove("active");
+    slidePara[count].classList.remove("active");
+    count = (count + 1) % slideImg.length;
+    slideImg[count].classList.add("active");
+    slidePara[count].classList.add("active");
+}
+
+function slideNext() {
+    slideImg[count].classList.remove("active");
+    slidePara[count].classList.remove("active");
+    count = (count - 1 + slideImg.length) % slideImg.length;
+    slideImg[count].classList.add("active");
+    slidePara[count].classList.add("active");
 }
